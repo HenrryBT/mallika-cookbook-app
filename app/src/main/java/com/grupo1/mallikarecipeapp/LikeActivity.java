6c919d38db11a3eebf099d66b06a553b6c000ec9
Henrry Beltran <henrrybt@gmail.com>
2022-10-04 00:03:38 -0500

    Addind some activities transitions

--- app/src/main/java/com/grupo1/mallikarecipeapp/LikeActivity.java
+++ app/src/main/java/com/grupo1/mallikarecipeapp/LikeActivity.java
@@ -2,13 +2,25 @@ package com.grupo1.mallikarecipeapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class LikeActivity extends AppCompatActivity {
+public class LikeActivity extends AppCompatActivity implements View.OnClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_like);
+
+        Button btnBack = findViewById(R.id.btn_back_liked);
+
+        btnBack.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        startActivity(new Intent(this, AccountActivity.class));
     }
 }
--- app/src/main/java/com/grupo1/mallikarecipeapp/MainActivity.java
+++ app/src/main/java/com/grupo1/mallikarecipeapp/MainActivity.java
@@ -2,13 +2,37 @@ package com.grupo1.mallikarecipeapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button btnOTD = findViewById(R.id.btn_send_otp);
+        Button btnApple = findViewById(R.id.btn_apple_login);
+        Button btnFacebook = findViewById(R.id.btn_facebook_login);
+        Button btnGoogle = findViewById(R.id.btn_google_login);
+
+        btnOTD.setOnClickListener(this);
+        btnApple.setOnClickListener(this);
+        btnFacebook.setOnClickListener(this);
+        btnGoogle.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        boolean otp = view.getId() == R.id.btn_send_otp;
+        boolean apple = view.getId() == R.id.btn_apple_login;
+        boolean facebook = view.getId() == R.id.btn_facebook_login;
+        boolean google = view.getId() == R.id.btn_google_login;
+
+        if (otp || apple || facebook || google)
+            startActivity(new Intent(this, HomeActivity.class));
     }
 }
